0d5f16ab2acbca2a0f709e7fb8164f9b5c55a346
Serdarq11 <serdarkaya.q@gmail.com>
2025-06-13 15:31:51 +0300

    Add files via upload

---
+++ _9_wrapperClasses.java
@@ -0,0 +1,10 @@
+public class _9_wrapperClasses {
+    public static void main(String[] args){
+
+        // Wrapper Classes : Allow primitive values (int, char double boolean) to be used as objects. (Wrap them in an object)
+        //                   Generally, don't wrap primitives unless you need an object.
+        //                   Allows use of collections Framework and static Utility Methods
+    }
+}
+
+// Watch this part of the video on YouTube!
